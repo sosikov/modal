7f42cdd963cc240eec83fb5ea957261d82930603
Nikita S <n.sosikov@bcconsolidator.com>
2024-01-30 13:10:27 +0100

    Inited the modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component, ComponentRef, EmbeddedViewRef, TemplateRef, createComponent } from '@angular/core';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'modal';
+  componentRef?: ComponentRef<ModalComponent>
+
+  constructor(private applicationRef: ApplicationRef) {}
+
+  show(template: TemplateRef<unknown>) {
+    const environmentInjector = this.applicationRef.injector;
+
+    this.componentRef = createComponent(ModalComponent, { environmentInjector });
+
+    Object.assign(this.componentRef.instance, { template });
+
+    const [rootNode] = (this.componentRef.hostView as EmbeddedViewRef<unknown>)
+      .rootNodes as HTMLElement[];
+
+    document.body.appendChild(rootNode);
+
+    this.applicationRef.attachView(this.componentRef.hostView);
+
+    document.body.classList.add('noscroll')
+  }
+
+  hide() {
+    if (this.componentRef) {
+      // this.applicationRef.detachView(this.componentRef.hostView);
+  
+      this.componentRef.destroy();
+
+      delete this.componentRef
+
+      // const [rootNode] = (this.componentRef.hostView as EmbeddedViewRef<unknown>)
+      //   .rootNodes as HTMLElement[];
+  
+      // rootNode.remove();
+
+      document.body.classList.remove('noscroll')
+    }
+  }
 }
